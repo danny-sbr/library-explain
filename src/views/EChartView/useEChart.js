592106c72f5f8e814d72afd5d36655678d5ebe85
danny_chen <danny_chen@seberon.com>
2024-11-25 16:48:01 +0800

    refactor: 優化 EChart 使用邏輯
    移除未使用的 `watch` 和 `ref`，簡化程式碼結構，並改進初始化與資源管理。

--- src/views/EChartView/useEChart.js
+++ src/views/EChartView/useEChart.js
@@ -1,59 +1,56 @@
-import { ref, nextTick, unref, onMounted, watch, onUnmounted } from 'vue'
+import { nextTick, unref, onMounted, onUnmounted } from 'vue'
 import * as echarts from 'echarts'
 
-export function useEChart(elRef, initialOption = {}, autoChartSize = true) {
+export function useEChart(elRef, initialOption = {}) {
   /* 這裡的 chartInstance 使用 let 方式宣告是因為避免 echart 實體被 vue 3 代理，
     因此使用 let 而不是使用 ref
     參見 https://github.com/apache/echarts/issues/17723
     參見 https://blog.csdn.net/youyudehan/article/details/135222342
   */
   let chartInstance
-  const option = ref(initialOption)
+  let resizeObserver
 
   // 初始化圖表
   const initChart = () => {
     const el = unref(elRef)
     if (!el) return
     chartInstance = echarts.init(el)
-    setOption(option.value)
+    setOption(initialOption)
   }
 
   // 設置圖表選項
   const setOption = newOption => {
-    option.value = newOption
     nextTick(() => {
       if (chartInstance) {
-        chartInstance.setOption(option.value)
+        chartInstance.setOption(newOption)
       } else {
         initChart()
       }
     })
   }
 
-  // 監聽選項變更，動態更新圖表
-  watch(option, newOption => {
-    setOption(newOption)
-  })
-
   function resize() {
     chartInstance?.resize()
   }
 
   function watchEl() {
-    const resizeObserver = new ResizeObserver(() => resize())
-    resizeObserver.observe(elRef.value)
+    if (!resizeObserver && elRef.value) {
+      resizeObserver = new ResizeObserver(() => resize())
+      resizeObserver.observe(elRef.value)
+    }
   }
 
-  // // 當元件掛載時初始化圖表並監聽視窗大小調整
+  // 當元件掛載時，初始化圖表並監聽視窗大小調整
   onMounted(() => {
     initChart()
-    window.addEventListener('resize', resize)
-    if (autoChartSize) watchEl()
+    watchEl()
   })
 
-  // 當元件卸載時移除監聽事件，並銷毀圖表實例
+  // 當元件卸載時，斷開 resizeObserver
   onUnmounted(() => {
-    window.removeEventListener('resize', resize)
+    if (resizeObserver) {
+      resizeObserver.disconnect()
+    }
   })
 
   return {
