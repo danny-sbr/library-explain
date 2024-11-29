c22a85e38f6bef99fb92c61e63414ab1eb2fad43
danny_chen <danny_chen@seberon.com>
2024-11-29 17:16:57 +0800

    feat: 新增 ConnectChart 元件至 EChartView，增強圖表互動性
    - 在 EChartView.vue 中新增 ConnectChart 元件，實現圖表之間的連接與互動功能。
    - 此改動提升了用戶在多圖表展示中的互動體驗。

--- src/views/EChartView/EChartView.vue
+++ src/views/EChartView/EChartView.vue
@@ -3,11 +3,13 @@ import ExplainEChart from './components/ExplainEChart.vue'
 import MyChart from './components/MyChart.vue'
 import SyncOneEventMultipleChart from './components/SyncOneEventMultipleChart.vue'
 import SyncOneEventTwoChart from './components/SyncOneEventTwoChart.vue'
+import ConnectChart from './components/ConnectChart.vue'
 </script>
 <template>
   <ExplainEChart />
   <MyChart />
   <SyncOneEventMultipleChart />
   <SyncOneEventTwoChart />
+  <ConnectChart />
 </template>
 <style scoped></style>
